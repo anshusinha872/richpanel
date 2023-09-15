225705e345490ef940fea747ed89713eee68cb0e
anshusinha872 <=>
2023-09-15 15:20:09 +0530

    done

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,18 +1,8 @@
 export const environment = {
-  APIEndpoint: 'http://localhost:3443/api/v1/', //Local
-  // APIEndpoint: 'https://instincts.co.in/api/v1/', //Local
+  // APIEndpoint: 'http://localhost:3443/api/v1/', //Local
+  APIEndpoint: 'https://instincts.co.in/api/v1/', //Local
   production: false,
   // apiUrl: 'http://localhost:3443/api/v1',
   apiUrl: 'http://localhost:4000',
   facebookAppId: '1526072548139602'
-  // APIEndpoint: 'https://invoicecraft.co.in/api/v1/', //Production
-  // firebaseConfig : {
-  //   apiKey: "AIzaSyAaYi_0X2PFjJN7-Qw0DKnaD1ibfFsQjSc",
-  //   authDomain: "toddle-c2ced.firebaseapp.com",
-  //   projectId: "toddle-c2ced",
-  //   storageBucket: "toddle-c2ced.appspot.com",
-  //   messagingSenderId: "303917694309",
-  //   appId: "1:303917694309:web:12568a4cae49b08f33f58c",
-  //   measurementId: "G-4DQEJF6VXN"
-  // }
 };
